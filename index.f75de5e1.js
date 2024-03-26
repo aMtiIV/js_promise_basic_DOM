7018381f91c04876bd8f51209d203cf729491519
Mateusz Oślizło <amti4osl@gmail.com>
2024-03-26 10:19:42 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+const logo = document.querySelector(".logo");
+const promise1 = new Promise((resolve, reject)=>{
+    logo.addEventListener("click", ()=>{
+        resolve();
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(reject, 3000);
+});
+const successHandler = ()=>{
+    const message = document.createElement("div");
+    message.className = "message";
+    message.textContent = "Promise was resolved!";
+    document.body.appendChild(message);
+};
+const errorHandler = ()=>{
+    const message = document.createElement("div");
+    message.className = "message error-message";
+    message.textContent = "Promise was rejected!";
+    document.body.appendChild(message);
+};
+promise1.then(successHandler).catch(errorHandler);
+promise2.then(successHandler).catch(errorHandler);
+
+//# sourceMappingURL=index.f75de5e1.js.map
